4fe3acef499649bd0ed4be07b2f25a341356224d
Akshat-66 <akshatripathi.1@gmail.com>
2025-08-15 20:24:57 +0530

    middleware done

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,24 @@
+// middleware/authMiddleware.js
+import jwt from "jsonwebtoken";
+
+export const protect = (req, res, next) => {
+  try {
+    // Get token from cookie
+    const token = req.cookies.jwt;
+
+    if (!token) {
+      return res.status(401).json({ message: "Not authorized, no token" });
+    }
+
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Attach user info to request
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({ message: "Not authorized, token invalid" });
+  }
+};
