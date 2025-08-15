178ef48259ec0105bad71af2721bce17f1701ad9
Akshat-66 <akshatripathi.1@gmail.com>
2025-08-16 04:59:51 +0530

    fds

--- server.js
+++ server.js
@@ -1,41 +1,41 @@
-import express from "express";
-import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
-import authRoutes from "./routes/auth.route.js";
-import { protect } from "./middleware/auth.middleware.js";
-import orderRoutes from "./routes/ship.route.js";
-import cors from "cors";
-import cookieParser from "cookie-parser"
-dotenv.config({ path: "./.env" });
+  import express from "express";
+  import dotenv from "dotenv";
+  import { connectDB } from "./config/db.js";
+  import authRoutes from "./routes/auth.route.js";
+  import { protect } from "./middleware/auth.middleware.js";
+  import orderRoutes from "./routes/ship.route.js";
+  import cors from "cors";
+  import cookieParser from "cookie-parser"
+  dotenv.config({ path: "./.env" });
 
-// Connect to MongoDB
-connectDB();
-const app = express();
-const PORT = process.env.PORT || 8000;
+  // Connect to MongoDB
+  connectDB();
+  const app = express();
+  const PORT = process.env.PORT || 8000;
 
-app.use(cookieParser())
+  app.use(cookieParser())
 
-const allowedOrigin = "https://shipsy-frontend-two.vercel.app/"; // frontend URL
+  const allowedOrigin = "https://shipsy-frontend-two.vercel.app"; // frontend URL
 
-app.use(cors({
-  origin: allowedOrigin,   // not '*'
-  credentials: true,       // allow cookies
-}));
+  app.use(cors({
+    origin: allowedOrigin,   // not '*'
+    credentials: true,       // allow cookies
+  }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+  });
 
-// Routes
-app.use(express.json()); // Move JSON middleware before routes
-app.use("/api/auth", authRoutes);
-app.use("/api/order", protect, orderRoutes);
+  // Routes
+  app.use(express.json()); // Move JSON middleware before routes
+  app.use("/api/auth", authRoutes);
+  app.use("/api/order", protect, orderRoutes);
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-  console.log(`📄 Swagger docs: http://localhost:${PORT}/api-docs`);
-});
+  // Start server
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+    console.log(`📄 Swagger docs: http://localhost:${PORT}/api-docs`);
+  });
